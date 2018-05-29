3d6ab8cdba5b4f3a4e9fe956056fe3f7d0bc0a7d
mizuff <flipflop.develop@gmail.com>
2018-05-29 18:28:11 +0900

    Refactor

--- search.py
+++ search.py
@@ -15,24 +15,15 @@ AS = 'access_token_secret'
 
 FILTER_URL = 'https://stream.twitter.com/1.1/statuses/filter.json'
 
-# params depend on OS
-CMD_NAME = ''
-IF_USES_SHELL = ''
+def usage():
+    print('Usage: python %s level name' % sys.argv[0])
+    print('Example: python %s 75 シュヴァリエ・マグナ' % sys.argv[0])
+    sys.exit()
 
-def set_os_params():
-    os_name = sys.platform
-    global CMD_NAME, IF_USES_SHELL
-
-    if os_name == 'win32':     # Windows
-        CMD_NAME = 'clip'
-        IF_USES_SHELL = True
-    elif os_name == 'darwin':    # Mac
-        CMD_NAME = 'pbcopy'
-        IF_USES_SHELL = False
-    else:
-        print("Don't understand this operating system.")
-        print("Try on Windows or Mac.")
-        sys.exit()
+def unsupported_os():
+    print("Don't understand this operating system.")
+    print("Try on Windows or Mac.")
+    sys.exit()
 
 # 文字列から参戦IDを抽出
 def parse(string):
@@ -43,43 +34,49 @@ def parse(string):
     else:
         return None
 
-# テキストをクリップボードにコピー
-def set_clipboard(string):
-    process = subprocess.Popen(CMD_NAME, stdin = subprocess.PIPE, shell=IF_USES_SHELL)
+# stringをクリップボードにコピー
+def set_clipboard(string, os_name):
+    if os_name == 'win32':
+        process = subprocess.Popen('clip', stdin = subprocess.PIPE, shell=True)
+    elif os_name == 'darwin':
+        process = subprocess.Popen('pbcopy', stdin = subprocess.PIPE, shell=False)
+    else:
+        unsupported_os()
     process.communicate(string.encode("utf-8")) # str型をbyte型に変換
 
-def usage():
-    print('Usage: python %s level name' % sys.argv[0])
-    print('Example: python %s 75 シュヴァリエ・マグナ' % sys.argv[0])
+def print_tweet(tweet):
+    tm = time.localtime()
+    name = tweet.get('user').get('name')
+    screen_name = tweet.get('user').get('screen_name')
+
+    print('[%02d:%02d:%02d] %s @%s' % (tm.tm_hour, tm.tm_min, tm.tm_sec, name, screen_name))
+    print(tweet.get('text') + '\n')    
 
 def main():
     try:
         args = sys.argv
         if len(args) != 3:
             usage()
-            sys.exit()
         
-        set_os_params()
+        os_name = sys.platform
+        if os_name != 'win32' and os_name != 'darwin':   # Windows / Mac
+            unsupported_os()
 
         # OAuth
         oauth_session = OAuth1Session(CK, CS, AT, AS)
         params = {'track': 'Lv%s %s' % (args[1], args[2])}
         req = oauth_session.post(FILTER_URL, params=params, stream=True)
+        
         for line in req.iter_lines():
             line_decode = line.decode('utf-8')
             if line_decode != '':   # if not empty
                 tweet = json.loads(line_decode)
+                # pass tweets via the game page
                 if tweet.get('source') == '<a href="http://granbluefantasy.jp/" rel="nofollow">グランブルー ファンタジー</a>':
-                    tm = time.localtime()
-                    name = tweet.get('user').get('name')
-                    screen_name = tweet.get('user').get('screen_name')
-
-                    print("[%02d:%02d:%02d] %s @%s" % (tm.tm_hour, tm.tm_min, tm.tm_sec, name, screen_name))
-                    print(tweet.get('text') + '\n')
-
                     raid_id = parse(tweet.get('text'))
                     if raid_id:
-                        set_clipboard(raid_id)
+                        set_clipboard(raid_id, os_name)
+                    print_tweet(tweet)
 
     except KeyboardInterrupt:
         print()
